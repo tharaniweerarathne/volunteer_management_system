f3f6aa9c8344ff2112896bf281187297e378beb2
Tharani Weerarathne <sasindikoralagamage@gmail.com>
2025-11-27 15:56:03 +0530

    Update contact form processing logic

--- presentation/process_contact.php
+++ presentation/process_contact.php
@@ -15,7 +15,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 
     $_SESSION['contact_result'] = $result;
-    header('Location: ../index.php#contact');
+    header("Location:index.php#contact");
     exit();
 }
 ?>
